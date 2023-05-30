63ef40fec038ec3b54feaf6a249fdf5863b9a2c6
Tayane <Tayane@DESKTOP-UJCUG10>
2023-05-30 16:12:44 -0300

    Adicionar arquivos de projeto.

---
+++ teste_claro_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("teste_claro_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("teste_claro_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("69BE816D-266C-4504-AB13-8347448F7D66")]
+[assembly: AssemblyVersion("1.0.0.0")]
